fce612c2df420d7f590fd07fe131e47fa6a863bb
Szymon Migas <ataszymon@gmail.com>
2024-12-21 02:27:56 +0100

    fonts, dependencies etc.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                "logo": ["Gloria Hallelujah"],
+                "main": ["Roboto"],
+            },
+            colors: {
+                "d-primary": "#2c2c2c",
+                "d-secondary": "#303030",
+                "d-text-primary": "#ccc",
+                "d-text-secondary": "#878787",
+            },
+        },
+    },
+    plugins: [],
+    darkMode: 'selector',
 }
 
