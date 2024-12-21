37193fd201b865b99e787d2bf18d5103b2e119e7
Szymon Migas <ataszymon@gmail.com>
2024-12-21 03:12:28 +0100

    added root path for gh-pages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import {BrowserRouter, Route, Routes} from "react-router";
 import Layout from "./Layout.tsx";
 import App from "./pages/HomePage.tsx";
+
+let root = ""
+// Uncomment this line to deploy to GitHub Pages
+//root = "wdai-project/"
 
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <Routes>
             <Route element={<Layout />} >
-                <Route path={'/'} element={<App/>}></Route>
+                <Route path={'/' + root} element={<App/>}></Route>
             </Route>
         </Routes>
     </BrowserRouter>
