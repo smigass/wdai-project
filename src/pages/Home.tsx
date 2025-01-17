d45e2073f7ded29f2ba206667e59997f316193b6
Szymon Migas <ataszymon@gmail.com>
2025-01-17 20:03:55 +0100

    created Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,29 @@
-
+import {useEffect, useState} from "react";
+import {getProducts} from "../Database/Products.ts";
+import ProductsInfo from "../interfaces/ProductsInfo.ts";
+import HomeSection from "../components/Home/HomeSection.tsx";
 
 function Home() {
+    const [products, setProducts] = useState<ProductsInfo>();
 
-  return (
-      <div className={'container-flex'}>
-      </div>
-  )
+    const fetchProducts = async () => {
+        const response = await getProducts()
+        setProducts(response)
+    }
+
+    useEffect(() => {
+        fetchProducts()
+    }, [])
+
+    return (
+        <div className={'container-flex'}>
+            {products == null ? <></> : Object.keys(products).map((key,i) => {
+                return (
+                    <HomeSection key={i} title={String(key).charAt(0).toUpperCase() + key.slice(1).toLowerCase()} products={products[key]}/>
+                )
+            })}
+        </div>
+    )
 }
 
 export default Home
